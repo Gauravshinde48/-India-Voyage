dc05c4f9c54223d216d395ec300808972b21f62c
Gaurav Vijay Shinde <gauravvshinde7@gmail.com>
2024-12-21 11:11:47 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,28 @@
+import Destination from "../components/Destination";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Trip from "../components/Trip";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        cName="hero"
+        heroImg="https://images.unsplash.com/photo-1483375801503-374c5f660610?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWlycGxhbmV8ZW58MHx8MHx8fDA%3D"
+        title="Your Gateway to Authentic Indian Adventures
+        "
+        text="Soul of India Travel"
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
+      <Destination />
+      <Trip />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
